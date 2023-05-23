1772f29a01633b2900fe09fee4a038c139ca5df7
Yavor Papazov <y_papaz@yahoo.com>
2023-05-23 14:55:42 -0500

    update gitignore

--- db/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const connectionString = 'mongodb+srv://yavor:yapap123@cluster0.eyyx1.mongodb.net/database-01?retryWrites=true&w=majority'
-
-const connectDB = () => {
-    return mongoose.connect(connectionString)
-}
-
-module.exports = connectDB
