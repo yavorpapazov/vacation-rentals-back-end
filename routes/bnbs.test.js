21b640e824feee6350191ea4d9ff526d98dbdfab
Yavor Papazov <y_papaz@yahoo.com>
2023-06-10 16:37:04 -0500

    commit

--- routes/bnbs.test.js
+++ routes/bnbs.test.js
@@ -9,6 +9,7 @@ const Item = require('../models/item');
 describe("/bnbs", () => {
     beforeAll(testUtils.connectDB);
     afterAll(testUtils.stopDB);
+    afterEach(testUtils.clearDB);
 
     const item = {
         bnbCity: 'Cozumel',
@@ -67,7 +68,6 @@ describe("/bnbs", () => {
       });
       //console.log(testBnbs)
     });
-    afterEach(testUtils.clearDB);
 
     describe("GET /", () => {
       it("should return all bnb items", async () => {
@@ -83,7 +83,6 @@ describe("/bnbs", () => {
 
     describe("GET /:id", () => {
       it("should return 404 if no matching id", async () => {
-        console.log(testBnbs)
         const res = await request(server).get("/bnbs/123");
         expect(res.statusCode).toEqual(404);
       });
@@ -94,6 +93,7 @@ describe("/bnbs", () => {
         expect(res.body).toMatchObject(item);
       });
     });
+
     describe('Before login', () => {
       describe('POST /', () => {
         it('should send 401 without a token', async () => {
@@ -107,6 +107,59 @@ describe("/bnbs", () => {
             .send(item);
           expect(res.statusCode).toEqual(401);
         });
+      });
+    });
+    describe('after login', () => {
+      const user0 = {
+        email: 'user0@mail.com',
+        password: '123password'
+      };
+      const user1 = {
+        email: 'user1@mail.com',
+        password: '456password'
+      }
+      let token0;
+      let token1;
+      beforeEach(async () => {
+        await request(server).post("/login/signup").send(user0);
+        const res0 = await request(server).post("/login").send(user0);
+        token0 = res0.body.token;
+        await request(server).post("/login/signup").send(user1);
+        const res1 = await request(server).post("/login").send(user1);
+        token1 = res1.body.token;
+        console.log(token0)
+        console.log(token1)
+      });
+      describe('POST /', () => {
+        it('should send 200', async () => {
+          let testBnb0 = {...testBnbs[0]}
+          delete testBnb0._id
+          console.log(testBnb0)
+          const res = await request(server)
+            .post("/bnbs")
+            .set('Authorization', 'Bearer ' + token0)
+            .send(testBnb0);
+          expect(res.statusCode).toEqual(200);
+          expect(res.body).toMatchObject(testBnb0)
+        });
+      //   it('should store note with userId', async () => {
+      //     await request(server)
+      //       .post("/notes")
+      //       .set('Authorization', 'Bearer ' + token0)
+      //       .send(note);
+      //     const user = await User.findOne({email: user0.email}).lean();
+      //     const savedNote = await Note.findOne({ userId: user._id }).lean();
+      //     expect(savedNote).toMatchObject(note);
+      //   });
+      //   it('should store note with userId for user1', async () => {
+      //     await request(server)
+      //       .post("/notes")
+      //       .set('Authorization', 'Bearer ' + token1)
+      //       .send(note2);
+      //     const user = await User.findOne({email: user1.email}).lean();
+      //     const savedNote = await Note.findOne({ userId: user._id }).lean();
+      //     expect(savedNote).toMatchObject(note2);
+      //   });
       });
     });
 });
